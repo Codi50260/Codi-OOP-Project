764ed8ec46d0276369994e4920e1f311a8db1343
Codi50260 <codi@trixta.com>
2022-04-10 12:23:27 +0200

    Updated Images Responsiveness

--- compare.php
+++ compare.php
@@ -1,10 +1,9 @@
 <?php
-
 class The_White_Rock_Hotel {
 	public $name = "The White Rock Hotel";
 	public $location = "Theodore Diligiannis 40, Greece";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
-	public $image = "images/White_Rock_Hotel.jpg";
+	public $image = "images/hotels/White_Rock_Hotel.jpg";
 	public $rate = 70;
 	public $facilities = 'Restaurant , Bar , Spa';
 	public $desc = "The White Rock Hotel is in the center of Athens, 5 minutes walking distance from Larissa railway station and Metaxourghio Metro station. The hotel boasts a roof garden from which you can relax and enjoy a splendid view of the city and the Acropolis. The roof garden is open from May to the end of September. Free Wi-Fi is also included.";
@@ -13,7 +12,7 @@ class Spotlight_Hotel {
 	public $name = "Spotlight Hotel";
 	public $location = "Panepistimiou 52, Greece";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
-	public $image = "images/Spotlight_Hotel.jpg";
+	public $image = "images/hotels/Spotlight_Hotel.webp";
 	public $rate = 100;
 	public $facilities = 'Restaurant , Bar , Swimming pool , Gym';
 	public $desc = "The Spotlight Hotel is located in the heart of the Historical Center of Athens and has recently been renovated. It's surrounded by the most important monuments of the Grecian History, such as the Acropolis, the New Museum of Acropolis, the Parliament, the Archaeological Museum, and the National Library. It's located within short distance to theaters, modern commercial shops and city malls, offering its guests a wide variety of sightseeing and entertainment choices.";
@@ -22,12 +21,11 @@ class Hotel_Bliss {
 	public $name = "Hotel Bliss";
 	public $location = "Via Aurelia 738, Italy";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
-	public $image = "images/Hotel_Bliss.jpg";
+	public $image = "images/hotels/Hotel_Bliss.jpg";
 	public $rate = 140;
 	public $facilities = 'Restaurant , Bar , Pick-up / Drop-off , Swimming pool , Spa , Gym';
 	public $desc = "Hotel Bliss is strategically located, less than 3 km away from Vatican City. Well connected by public transport. Decorated & furnished in a modern elegant style, our rooms and facilities are fresh & stylish, yet maintaining a coziness & warmth that are essential to us. Extra comfort is guaranteed by our hi-speed Wi-Fi, satellite HD LCD TVs, sound-proof double windows, and other facilities.";
 }
-
 ?>
 <html class="no-js">
 	<head>
@@ -135,7 +133,7 @@ class Hotel_Bliss {
 			<div class="row">
 				<div class="col-md-6">
 					<?php
-						echo "<div class='fh5co-map'><img src=".$hotel->image." alt=".$_POST['Hotel_Name']."></div>";
+						echo "<div class='fh5co-map'><img src=".$hotel->image." alt=".$_POST['Hotel_Name']." style='max-width:100%; max-height:100%; object-fit: contain'></div>";
 					?>
 				</div>
 				<div class="col-md-6">
@@ -178,7 +176,7 @@ class Hotel_Bliss {
 			<div class="row">
 				<div class="col-md-6">
 					<?php
-						echo "<div class='fh5co-map'><img src=".$hotel->image." alt=".$_POST['Hotel_Name']."></div>";
+						echo "<div class='fh5co-map'><img src=".$hotel2->image." alt=".$_POST['Hotel_Name']." style='max-width:100%; max-height:100%; object-fit: contain'></div>";
 					?>
 				</div>
 				<div class="col-md-6">
