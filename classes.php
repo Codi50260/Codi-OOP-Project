ad5b2cc24a92c6c49829be82c78973052c83afc0
Codi50260 <codi@trixta.com>
2022-04-11 19:47:33 +0200

    Updated Variables

--- classes.php
+++ classes.php
@@ -4,26 +4,80 @@ class The_White_Rock_Hotel {
 	public $location = "Theodore Diligiannis 40, Greece";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
 	public $image = "images/hotels/White_Rock_Hotel.jpg";
+	public $resImage = "images/WhiteRock_Restaurant.jpg";
+	public $barImage = "images/WhiteRock_Bar.jpg";
+	public $spaImage = "images/WhiteRock_Spa.jpg";
 	public $rate = 70;
 	public $facilities = 'Restaurant , Bar , Spa';
 	public $desc = "The White Rock Hotel is in the center of Athens, 5 minutes walking distance from Larissa railway station and Metaxourghio Metro station. The hotel boasts a roof garden from which you can relax and enjoy a splendid view of the city and the Acropolis. The roof garden is open from May to the end of September. Free Wi-Fi is also included.";
+    public $restaurant = "Here we believe in a good start to the day, which is why here a rushed breakfast, lunch or dinner is unheard of. You can enjoy our buffet of wholesome, good quality food which will give you a nutritious boost, to enjoy your day outdoors with just the right amount of energy.";
+	public $resHour = "8:00 AM - 12:00 PM <br> 17:00 PM - 21:00 PM";
+	public $resType = "Buffet";
+	public $bar = "White Rock Hotel Bar.";
+	public $barHour = "10:00 AM - 10:00 PM";
+	public $barType = "Pub";
+	public $spa = "White Rock Spa.";
+	public $spaHour = "Appointment Only";
+	public $spaType = "Qualified Massage Therapy";
 }
+
 class Spotlight_Hotel {
 	public $name = "Spotlight Hotel";
 	public $location = "Panepistimiou 52, Greece";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
 	public $image = "images/hotels/Spotlight_Hotel.webp";
+	public $resImage = "images/Spotlight_Restaurant.jpg";
+	public $barImage = "images/Spotlight_Bar.jpeg";
+	public $poolImage = "images/Spotlight_Pool.jpg";
+	public $gymImage = "images/Spotlight_Gym.jpg";
 	public $rate = 100;
 	public $facilities = 'Restaurant , Bar , Swimming pool , Gym';
 	public $desc = "The Spotlight Hotel is located in the heart of the Historical Center of Athens and has recently been renovated. It's surrounded by the most important monuments of the Grecian History, such as the Acropolis, the New Museum of Acropolis, the Parliament, the Archaeological Museum, and the National Library. It's located within short distance to theaters, modern commercial shops and city malls, offering its guests a wide variety of sightseeing and entertainment choices.";
+	public $restaurant = "Are you looking for an unforgettable culinary experience? Spotlight Hotel has an extraordinary dining experience for you. At the Chef's Table, you are overlooking the heart of the kitchen Arjan Speelman. While the chefs prepare the dishes in the utmost concentration, you will not miss out on the breathtaking view or the excellent service that we have to offer you.";
+	public $resHour = "7:00 AM - 10:00 AM <br> 13:00 PM - 15:00 PM <br> 18:00 PM - 21:00 PM";
+	public $resType = "Fine Dining";
+	public $bar = "Spotlight Hotel Bar.";
+	public $barHour = "16:00 PM - 10:00 PM";
+	public $barType = "Formal Bar";
+	public $pool = "Spotlight Hotel Pool.";
+	public $poolHour = "10:00 AM - 17:00 PM";
+	public $poolType = "Communal Indoor Pool";
+	public $gym = "Spotlight Hotel Gym.";
+	public $gymHour = "Appointment Only";
+	public $gymType = "Crossfit";
 }
+
 class Hotel_Bliss {
 	public $name = "Hotel Bliss";
 	public $location = "Via Aurelia 738, Italy";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
 	public $image = "images/hotels/Hotel_Bliss.jpg";
+	public $resImage = "images/Bliss_Restaurant.jpg";
+	public $barImage = "images/Bliss_Bar.jpg";
+	public $valetImage = "images/Bliss_Valet.jpg";
+	public $poolImage = "images/Bliss_Pool.webp";
+	public $spaImage = "images/Bliss_Spa.jpg";
+	public $gymImage = "images/Bliss_Gym.webp";
 	public $rate = 140;
 	public $facilities = 'Restaurant , Bar , Pick-up / Drop-off , Swimming pool , Spa , Gym';
 	public $desc = "Hotel Bliss is strategically located, less than 3 km away from Vatican City. Well connected by public transport. Decorated & furnished in a modern elegant style, our rooms and facilities are fresh & stylish, yet maintaining a coziness & warmth that are essential to us. Extra comfort is guaranteed by our hi-speed Wi-Fi, satellite HD LCD TVs, sound-proof double windows, and other facilities.";
+    public $restaurant = "Hotel Bliss offers elegant yet accessible cuisine that builds on products at their best. Together with the characteristic acids and spices of Jacob Jan Boerma, they give substance to the illusion of white. Recognizable flavors return in an unexpected way.";
+	public $resHour = "7:00 AM - 10:00 PM";
+	public $resType = "World Class Restaurant";
+	public $bar = "Hotel Bliss Bar.";
+	public $barHour = "10:00 AM - 12:00 AM";
+	public $barType = "Formal Bar";
+	public $parking = "Hotel Bliss Pick-Up / Drop-Off.";
+	public $parkingHour = "8:00 AM - 8:00 PM";
+	public $parkingType = "Valet Parking";
+	public $pool = "Hotel Bliss Pool.";
+	public $poolHour = "7:00 AM - 18:00 PM";
+	public $poolType = "Collection of Indoor and Outdoor Heated Pools";
+	public $spa = "Hotel Bliss Spa.";
+	public $spaHour = "11:00 AM - 16:00 PM";
+	public $spaType = "Top 5 Best Masseuses and Masseurs";
+	public $gym = "Hotel Bliss Gym.";
+	public $gymHour = "8:00 AM - 8:00 PM";
+	public $gymType = "Elite Trainers";
 }
 ?>
