48bb6a0c0de4758e9fc870e1ab3419338ba15294
Codi50260 <codi@trixta.com>
2022-04-14 21:50:34 +0200

    Final touches

--- classes.php
+++ classes.php
@@ -1,6 +1,6 @@
 <?php
-class The_White_Rock_Hotel {
-	public $name = "The White Rock Hotel";
+class Prestige_Hotel {
+	public $name = "Prestige Hotel";
 	public $location = "Theodore Diligiannis 40, Greece";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
 	public $image = "images/hotels/White_Rock_Hotel.jpg";
@@ -8,10 +8,10 @@ class The_White_Rock_Hotel {
 	public $barImage = "images/WhiteRock_Bar.jpg";
 	public $spaImage = "images/WhiteRock_Spa.jpg";
 	public $rate = 70;
-	public $facilities = 'Restaurant , Bar , Spa';
-	public $desc = "The White Rock Hotel is in the center of Athens, 5 minutes walking distance from Larissa railway station and Metaxourghio Metro station. The hotel boasts a roof garden from which you can relax and enjoy a splendid view of the city and the Acropolis. The roof garden is open from May to the end of September. Free Wi-Fi is also included.";
+	public $facilities = 'Restaurant, Bar, Spa';
+	public $desc = "The Prestige Hotel is in the center of Athens, 5 minutes walking distance from Larissa railway station and Metaxourghio Metro station. The hotel boasts a roof garden from which you can relax and enjoy a splendid view of the city and the Acropolis. The roof garden is open from May to the end of September. Free Wi-Fi is also included.";
     public $restaurant = "Here we believe in a good start to the day, which is why here a rushed breakfast, lunch or dinner is unheard of. You can enjoy our buffet of wholesome, good quality food which will give you a nutritious boost, to enjoy your day outdoors with just the right amount of energy.";
-	public $resHour = "8:00 AM - 12:00 PM <br> 17:00 PM - 21:00 PM";
+	public $resHour = "08:00 AM - 15:00 PM <br> 17:00 PM - 22:00 PM";
 	public $resType = "Buffet";
 	public $bar = "We have a lovely old pub with wood panelling, a mezzanine bar and wooden barrels over the bar. The famous Pieter Hoppe opened his liqueur distillery here in around 1780 and the premises has been serving jenever and beers here ever since.";
 	public $barHour = "10:00 AM - 10:00 PM";
@@ -31,13 +31,13 @@ class Spotlight_Hotel {
 	public $poolImage = "images/Spotlight_Pool_2.jpg";
 	public $gymImage = "images/Spotlight_Gym_2.jpg";
 	public $rate = 100;
-	public $facilities = 'Restaurant , Bar , Swimming pool , Gym';
+	public $facilities = 'Restaurant, Bar, Swimming Pool, Gym';
 	public $desc = "The Spotlight Hotel is located in the heart of the Historical Center of Athens and has recently been renovated. It's surrounded by the most important monuments of the Grecian History, such as the Acropolis, the New Museum of Acropolis, the Parliament, the Archaeological Museum, and the National Library. It's located within short distance to theaters, modern commercial shops and city malls, offering its guests a wide variety of sightseeing and entertainment choices.";
 	public $restaurant = "Are you looking for an unforgettable culinary experience? Spotlight Hotel has an extraordinary dining experience for you. At the Chef's Table, you are overlooking the heart of the kitchen Arjan Speelman. While the chefs prepare the dishes in the utmost concentration, you will not miss out on the breathtaking view or the excellent service that we have to offer you.";
-	public $resHour = "7:00 AM - 10:00 AM <br> 13:00 PM - 15:00 PM <br> 18:00 PM - 21:00 PM";
+	public $resHour = "07:00 AM - 10:00 AM <br> 13:00 PM - 15:00 PM <br> 18:00 PM - 23:00 PM";
 	public $resType = "Fine Dining";
 	public $bar = "You will find a vibrant spot here, filled with culture and taste. At daytime, we offer you the best spot for appointments, meetings or a little catch-up. However, as the sun sets and the volume goes up, our DJs and cocktail shakers turn it into an energetic lounge bar.";
-	public $barHour = "16:00 PM - 10:00 PM";
+	public $barHour = "16:00 PM - 22:00 PM";
 	public $barType = "Formal Bar";
 	public $pool = "This is a spacious and tranquil urban retreat, where visitors can enjoy a dazzling indoor communal pool which is maintained at the highest level.";
 	public $poolHour = "10:00 AM - 17:00 PM";
@@ -52,32 +52,32 @@ class Hotel_Bliss {
 	public $location = "Via Aurelia 738, Italy";
 	public $review = "<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>";
 	public $image = "images/hotels/Hotel_Bliss.jpg";
-	public $resImage = "images/Bliss_Restaurant_2.jpg";
+	public $resImage = "images/Bliss_Restaurant_3.jpg";
 	public $barImage = "images/Bliss_Bar.jpg";
 	public $valetImage = "images/Bliss_Valet.jpg";
 	public $poolImage = "images/Bliss_Pool.webp";
 	public $spaImage = "images/Bliss_Spa.jpg";
 	public $gymImage = "images/Bliss_Gym.jpg";
 	public $rate = 140;
-	public $facilities = 'Restaurant , Bar , Pick-up / Drop-off , Swimming pool , Spa , Gym';
+	public $facilities = 'Restaurant, Bar, Pick-up / Drop-off, Swimming Pool, Spa, Gym';
 	public $desc = "Hotel Bliss is strategically located, less than 3 km away from Vatican City. Well connected by public transport. Decorated & furnished in a modern elegant style, our rooms and facilities are fresh & stylish, yet maintaining a coziness & warmth that are essential to us. Extra comfort is guaranteed by our hi-speed Wi-Fi, satellite HD LCD TVs, sound-proof double windows, and other facilities.";
     public $restaurant = "Hotel Bliss offers elegant yet accessible cuisine that builds on products at their best. Together with the characteristic acids and spices of Jacob Jan Boerma, they give substance to the illusion of white. Recognizable flavors return in an unexpected way.";
-	public $resHour = "7:00 AM - 10:00 PM";
+	public $resHour = "7:00 AM - 23:00 PM";
 	public $resType = "World Class Restaurant";
 	public $bar = "The Lobby stands for good food, nice drinks and undiluted 'gezelligheid'. This is where the best city vibes come together at the bar, at the table, on your plate and in your glass. Service is our priority, and our people are happy to help you. So that you can be completely yourself.";
 	public $barHour = "10:00 AM - 12:00 AM";
 	public $barType = "Formal Bar";
 	public $parking = "Hotel Bliss offers a professional valet company to relieve you of the hassle of parking your vehicle on arrival, your vehicle will be transferred to safe and secured underground parking five minutes walk from the hotel. ";
-	public $parkingHour = "8:00 AM - 8:00 PM";
+	public $parkingHour = "08:00 AM - 20:00 PM";
 	public $parkingType = "Valet Parking";
 	public $pool = "Swim from the heated indoor pool right into the open air and float around and observe the beautiful views. You can fully relax and enjoy the warm, soothing water in our 20-meter pool. Swimming is good for the muscles and contributes to a healthy blood circulation.";
-	public $poolHour = "7:00 AM - 18:00 PM";
+	public $poolHour = "07:00 AM - 18:00 PM";
 	public $poolType = "Collection of Indoor and Outdoor Heated Pools";
 	public $spa = "Welcome to an escape within an escape, where we take you from the everyday life into an insider scene of surprising sensations. Find your balance, boost your stamina and relax. We have you covered.";
 	public $spaHour = "11:00 AM - 16:00 PM";
 	public $spaType = "Top 5 Best Masseuses and Masseurs";
 	public $gym = "All hotel guests are welcome at our gym. Our fitness equipment complies with the latest health regulations and is easy to use. Here you will find various cardio equipment, including a cross trainer and treadmill.";
-	public $gymHour = "8:00 AM - 8:00 PM";
+	public $gymHour = "08:00 AM - 20:00 PM";
 	public $gymType = "Elite Trainers";
 }
 ?>
